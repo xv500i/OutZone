4818444384d68b6cd908fea4dde25e182d8e544a
natxoraga22 <natxoraga22@gmail.com>
2013-03-31 01:15:37 +0100

    GameSound intentat, revisal!!

--- GameSound.h
+++ GameSound.h
@@ -1,15 +1,29 @@
-#pragma once
 
-#include <windows.h>
-#include <stdio.h>
-#include <conio.h>
+#pragma once
+#pragma comment(lib, "fmodex_vc")
+
 #include "fmod.hpp"
 #include "fmod_errors.h"
 
+
 class GameSound
 {
+private:
+	FMOD::System *system;
+	FMOD::Sound *sound;
+	FMOD::Channel *channel;
+
+	void errorCheck(FMOD_RESULT result);
+
 public:
 	GameSound(void);
 	~GameSound(void);
+
+	/* Loading */
+	bool load(const char *filename, const char *ext);
+
+	/* Playing */
+	void play();
+	void stop();
 };
 
