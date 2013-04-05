6b1f9252af197016c6f6084e6e00adfd9e8d7fd9
Alex <xv500i@gmail.com>
2013-04-05 08:34:24 +0200

    asddsa

--- GameData.cpp
+++ GameData.cpp
@@ -21,7 +21,7 @@ GameData::~GameData(void) {}
 /* Textures */
 bool GameData::loadTextures()
 {
-	const char* filenames[NUM_TEXTURES] = {"title", "instructions", "pause", "gameover", "button_quit", "button_start","button_help", "button_restart", "button_mainmenu", "cursor", "lantern", "manual"};
+	const char* filenames[NUM_TEXTURES] = {"title", "instructions", "pause", "gameover", "button_quit", "button_start","button_help", "button_restart", "button_mainmenu", "cursor", "lantern", "manual", "boss", "tentacle_piece", "tentacle_base", "tentacle_weapon"};
 	for (unsigned int i = 0; i < textures.size(); i++) {
 		bool b = textures[i].load(filenames[i], TEXTURE_EXT);
 		if (!b) return false;
--- GameData.h
+++ GameData.h
@@ -12,7 +12,7 @@ class GameData
 {
 private:
 	/* Textures */
-	const static int NUM_TEXTURES = 12;		// TOCHANGE: modificar depenent del numero de textures carregades
+	const static int NUM_TEXTURES = 16;		// TOCHANGE: modificar depenent del numero de textures carregades
 	const static char* TEXTURE_EXT;
 	
 	/* TileSheets */
@@ -47,6 +47,10 @@ public:
 	const static int CURSOR_INDEX = 9;
 	const static int LANTERN_INDEX = 10;
 	const static int MANUAL_INDEX = 11;
+	const static int BOSS_TEX_INDEX = 12;
+	const static int TENTACLE_BASE_TEX_INDEX = 13;
+	const static int TENTACLE_TEX_INDEX = 14;
+	const static int TENTAGLE_GUN_TEX_INDEX = 15;
 
 	/* TileSheet indexes */		// TOCHANGE: afegir indexs de tots els tile sheets carregats
 	const static int LEVEL1_TILES_INDEX = 0;
